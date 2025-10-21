3f5f1009d942c8e77d3e3152212354c1701e14d5
darwinst <darwin_moraga@fet.edu.co>
2025-10-20 23:59:57 -0500

    dependencia faltante

--- FactoryMethod2/src/main/java/org/example/factorymethod2/solution/ExportController.java
+++ FactoryMethod2/src/main/java/org/example/factorymethod2/solution/ExportController.java
@@ -1,6 +1,9 @@
 package org.example.factorymethod2.solution;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExportController {
